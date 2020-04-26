2b33b68a5bc4f811f7abacc7ec29fcaa390641b0
AlexisIBr19 <alexiscalderon123@hotmail.com>
2020-04-26 18:59:26 -0500

    basic template about info of the user

--- src/Component/Profile.js
+++ src/Component/Profile.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React from "react";
 
-class Profile extends React.Component{
-    render(){
-        return(
-            <div>
-                <h1>
-                    asdasd
-                </h1>
-            </div>
-        )
-    }
-
+class Profile extends React.Component {
+  state = {
+    //well know function from magic functions
+    profile: null,
+    error: null,
+  };
+  componentDidMount() {
+    this.loadUserProfile();
+  }
+  loadUserProfile() {
+    this.props.auth.getProfile((profile, error) => {
+      this.setState({ profile, error });
+    });
+  }
+  render() {
+    const { profile } = this.state;
+    if (!profile) return null;
+    //when using componentdidmount, o sea :v, when waiting for other data to come from async functions its better to check if there is already data
+    return (
+      <div>
+        <h1>{profile.nickname}</h1>
+        <img
+          style={{ maxWidth: 50, maxHeight: 50 }}
+          src={profile.picture}
+          alt="profile pic"
+        ></img>
+        <pre>{JSON.stringify(profile, null, 2)}</pre>
+      </div>
+    );
+  }
 }
-export default Profile
+export default Profile;
