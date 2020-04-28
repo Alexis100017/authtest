a35f02f6de798884784048a016345fc6bfa6a43b
AlexisIBr19 <alexiscalderon123@hotmail.com>
2020-04-28 11:53:10 -0500

    fetch public api

--- src/Public.js
+++ src/Public.js
@@ -1,21 +1,22 @@
 import React from "react";
-import { response } from "express";
 
 class Public extends React.Component {
   state = {
     message: "",
   };
+
   componentDidMount() {
-    fetch("/public")
+    fetch("/public") //we are telling to go to /public to fetch all data available
       .then((response) => {
-        if (response.ok) return response.json;
+        if (response.ok) return response.json();
         throw new Error("Network response was not ok.");
       })
       .then((response) => this.setState({ message: response.message }))
       .catch((error) => this.setState({ message: error.message }));
   }
+
   render() {
-    return <h1>{this.state.message}</h1>;
+    return <p>{this.state.message}</p>;
   }
 }
 
